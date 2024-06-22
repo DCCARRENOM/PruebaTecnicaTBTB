83d57a5e1c3e28b36d5259c6530600457fbea8f0
Daniel Carreño <danielcarreno0102@gmail.com>
2024-06-21 20:03:53 -0500

    Creadas clases ClientRepository y ClientService

--- src/main/java/PruebaTecnicaTBTBcom/example/PruebaTecnicaTBTB/crudRepository/ClientRepository.java
+++ src/main/java/PruebaTecnicaTBTBcom/example/PruebaTecnicaTBTB/crudRepository/ClientRepository.java
@@ -7,5 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 //Un repository ayuda a hacer operaciones sobre una base de datos de manera sencilla
 //al heredar de CrudRepository se pasan como argumentos la tabla que se va a consultar y el tipo de su llave primaria
 public interface ClientRepository  extends CrudRepository<Client, Integer> {
+    //Se crea un query method para colocar una sentencia sql personalizada para actulizar los datos de un cliente
+    @Query(value = "UPDATE clients SET clientName=?1, lastName=?2, address=?3 WHERE clients.id=?4 ", nativeQuery = true)
+    void updateClient (String clientName, String lastName, String address, int id);
 
 }
--- src/main/java/PruebaTecnicaTBTBcom/example/PruebaTecnicaTBTB/repository/ClientService.java
+++ src/main/java/PruebaTecnicaTBTBcom/example/PruebaTecnicaTBTB/repository/ClientService.java
@@ -10,5 +10,29 @@ import java.util.Optional;
 
 @Repository
 public class ClientService {
+    @Autowired //Para inyectar el repository al servicio
+    private ClientRepository clientRepository;
+
+    //Método para obtener todos los registros de la tabla
+    public List<Client> getAll(){
+        return (List<Client>) clientRepository.findAll();
+    }
+
+    public Optional<Client> findById(int idClient){
+        return clientRepository.findById(idClient);
+    }
+
+    public Client save(Client client){
+        return clientRepository.save(client);
+    }
+
+    public void delete (int idClient){
+        clientRepository.deleteById(idClient);
+    }
+
+    public String updateClient (String clientName, String lastName, String address, int id){
+        clientRepository.updateClient(clientName,lastName,address,id);
+        return "Cliente Actualizado";
+    }
 
 }
