ecef32e8669198b484c282d02517c548cce2ee61
Daniel Carreño <danielcarreno0102@gmail.com>
2024-06-21 19:32:43 -0500

    Creada clase Client

--- src/main/java/PruebaTecnicaTBTBcom/example/PruebaTecnicaTBTB/model/Client.java
+++ src/main/java/PruebaTecnicaTBTBcom/example/PruebaTecnicaTBTB/model/Client.java
@@ -6,4 +6,49 @@ import jakarta.persistence.*;
 @Table(name = "clients") //Se especifica el nombre de la tabla que se va a mapear
 public class Client {
 
+    @Id //Se usa la anotación Id para indicar que este atributo es la clave primaria de la tabla
+    @Column(name = "id") //Se especifica que columna de la tabla mapea este atributo
+    private Integer id;
+
+    @Column(name = "clientname")
+    private String clientName;
+
+    @Column (name = "lastname")
+    private String lastName;
+
+    @Column (name = "address")
+    private String address;
+
+    //Getter and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
