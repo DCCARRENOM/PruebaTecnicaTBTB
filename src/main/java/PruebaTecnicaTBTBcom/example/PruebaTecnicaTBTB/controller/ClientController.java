d0030f249d5baeab34f28f227f46f599d98c14f3
Daniel Carreño <danielcarreno0102@gmail.com>
2024-06-21 20:42:44 -0500

    Terminada clase ClientController

--- src/main/java/PruebaTecnicaTBTBcom/example/PruebaTecnicaTBTB/controller/ClientController.java
+++ src/main/java/PruebaTecnicaTBTBcom/example/PruebaTecnicaTBTB/controller/ClientController.java
@@ -13,5 +13,36 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/client") //La ruta utilizada para consultar la API
 public class ClientController {
+    @Autowired //Para inyectar el servicio en el controlador
+    private ClientService clientService;
 
+    @GetMapping("/all") //Se especifica el tipo de request y la ruta donde se aloja el endpoint
+    public List<Client> findAll(){
+        return clientService.getAll();
+    };
+
+    @GetMapping("/{id}")
+    public Optional<Client> getClient(@PathVariable("id") int idClient) {
+        return clientService.findById((idClient));
+    };
+
+    @DeleteMapping("/delete/{id}")
+    public void delete (@PathVariable("id") int idClient){
+        clientService.delete(idClient);
+    }
+
+    @PostMapping("/save")
+    public Client save(@RequestBody Client client){
+        return clientService.save(client);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateClient(@RequestBody Client client){
+        try {
+            clientService.updateClient(client.getClientName(), client.getLastName(), client.getAddress(), client.getId());
+        }catch(Exception e){
+            return new ResponseEntity<String>("Cliente Actualizado", HttpStatus.OK);
+        }
+        return null;
+    }
 }
